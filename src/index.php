3c1c3602bf4216b3650a83835cdd1c40ebb63b08
Dawid Schmidt <dawid.schmidt@kreolabs.com>
2018-07-23 18:21:42 +0200

    host

--- src/index.php
+++ src/index.php
@@ -15,10 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple SYNTHRONE PHP APP - MAIN SUPER SERVICE</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <h1>Synthrone Microservice - Main</h1>
+                <p>Host: <?php echo gethostname(); ?></p>
+                <p>PHP version: <?php echo phpversion(); ?></p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
